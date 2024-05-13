43e783050f4f144ee9c9f9b09552a12752843e47
nicoguerra18 <139820627+nicoguerra18@users.noreply.github.com>
2024-05-13 12:38:07 -0400

    added navbar and button styles

--- grid-game/src/App.js
+++ grid-game/src/App.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
 import PokeHopper from "./components/PokeHopper";
 import PokemonPicker from "./components/PokemonPicker";
-import World from "./components/World";
+import { Button } from "react-bootstrap";
 
 function App() {
   const [selectedPokemon, setSelectedPokemon] = useState(null);
 
   return (
     <div className="App">
+      <NavBar />
+
       {/* <PokemonPicker setSelectedPokemon={setSelectedPokemon} /> */}
+
       <PokeHopper />
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <button className="btn-add">Choose Pokemon</button>
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <h1>Pokémon Hopper</h1>
     </div>
   );
 }
